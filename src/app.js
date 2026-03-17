d636b23684c75542a4ce07301a7e248145a9c12c
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-03-18 00:20:53 +0600

    cloud service provider + backend

--- src/app.js
+++ src/app.js
@@ -1,52 +1,36 @@
 const express = require("express");
-const noteModel = require("./models/note.model");
-require("dotenv").config();
+const multer = require("multer");
+const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model");
 
 const app = express();
 app.use(express.json());
 
-app.post("/notes", async (req, res) => {
-  const data = req.body;
-  await noteModel.create({
-    title: data.title,
-    description: data.description,
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post("/create-post", upload.single("image"), async (req, res) => {
+  const response = await uploadFile(req.file.buffer);
+
+  const post = await postModel.create({
+    image: response.url,
+    caption: req.body.caption,
   });
 
-  res.status(201).json({
-    message: "note created",
-  });
+  return res.status(201).json({
+    message: "post created successfully",
+    post 
+  })
 });
 
-app.get("/notes", async (req, res) => {
-  const notes = await noteModel.find();
+app.get("/post",async (req, res) => {
+    const posts = await postModel.find()
 
-  res.status(200).json({
-    message: "notes fetched successfully",
-    notes: notes,
-  });
-});
-
-app.delete("/notes/:id", async (req, res) => {
-  const id = req.params.id;
-
-  await noteModel.findOneAndDelete({
-    _id: id,
+    return res.status(200).json({
+    message: "post fetched successfully",
+    posts
   });
 
-  res.status(200).json({
-    message: "note deleted successfully",
-  });
-});
 
-app.patch("/notes/:id", async (req, res) => {
-  const id = req.params.id;
-  const description = req.body.description;
-
-  await noteModel.findOneAndUpdate({ _id: id }, { description: description });
-
-  res.status(200).json({
-    message: "note updated successfully",
-  });
-});
+})
 
 module.exports = app;
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 async function connectDB() {
-    await mongoose.connect(`mongodb+srv://rakib343:${process.env.MONGODB_PASSWORD}@cluster0.buo9k6z.mongodb.net/learning`)
+    await mongoose.connect(`mongodb+srv://rakib343:e1nUutNtw0z1rz9L@cluster0.buo9k6z.mongodb.net/project-1`)
     console.log("database is connected");
 }
 
