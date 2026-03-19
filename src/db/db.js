73bcc28fac4003624e5e9d2f50286835a0295843
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-03-19 12:07:44 +0600

    project integration

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 async function connectDB() {
-    await mongoose.connect(`mongodb+srv://rakib343:e1nUutNtw0z1rz9L@cluster0.buo9k6z.mongodb.net/project-1`)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("database is connected");
 }
 
